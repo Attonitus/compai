1f8100302441155aa19845828611079e681676a2
Jesus Antonio <valenzuelarodriguezj512@gmail.com>
2026-05-01 13:29:43 -0500

    aws env changed

--- app/actions/upload-action.ts
+++ app/actions/upload-action.ts
@@ -6,7 +6,7 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 const client = new S3Client({
     region: process.env.AWS_REGION!,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY!,
+        accessKeyId: process.env.AWS_BUCKET_ACCESS_KEY!,
         secretAccessKey: process.env.AWS_SECRET_KEY!,
     },
 });
